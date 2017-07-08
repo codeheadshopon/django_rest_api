cd5d6daa010625bec5924317eaa70f729164feb1
Md Shopon <shopon.uap@gmail.com>
2017-07-09 00:20:02 +0600

    Update views.py

--- rest_api/views.py
+++ rest_api/views.py
@@ -40,7 +40,7 @@ def MODEL(image):
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
-                     input_shape=(1, 56, 56)))
+                     input_shape=input_shape))
     model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
