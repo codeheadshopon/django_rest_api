9566a980b11610f28c7c6a91a666bfd0f8dfa871
Md Shopon <shopon.uap@gmail.com>
2017-07-11 00:50:09 +0600

    NewWeight

--- imageupload/Final_Model.py
+++ imageupload/Final_Model.py
@@ -1,62 +1,45 @@
 from __future__ import print_function
-import numpy as np
-import cPickle,gzip,sys
 
-np.random.seed(1337)
-import keras
-from keras.callbacks import ModelCheckpoint
+import numpy as np
+
+np.random.seed(1337)  # for reproducibility
+
+import cv2
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-img_rows, img_cols = 56, 56
-nb_filters = 16
-pool_size = 2
-kernel = 3
-nb_channels = 1
-nb_class = 60
-batch_size = 32
-epochs = 5
+import keras.backend as K
+import keras
+from keras.callbacks import ModelCheckpoint
+from PIL import Image
 
+image='/root/projects/django_rest_api/uploaded_media/b_2.jpg'
+#image='/home/codehead/Downloads/django_rest_api/uploaded_media/b_2.jpg'
+#img = cv2.imread(image)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#print(img.shape)
+#img = cv2.resize(img, (56, 56))
 
+img = Image.open(image).convert('L')
+img = img.resize((56, 56), Image.ANTIALIAS)
 
-def dataset_load(path):
-    if path.endswith(".gz"):
-        f=gzip.open(path,'rb')
-    else:
-        f=open(path,'rb')
-
-    if sys.version_info<(3,):
-        data=cPickle.load(f)
-    else:
-        data=cPickle.load(f,encoding="bytes")
-    f.close()
-    return data
-
-(x_train,y_train),(x_test,y_test) = dataset_load('./BanglaLekha_Basic_Numerals.pkl.gz')
-
-
+img = np.array(img)
+print(img.shape)
 if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+    input_shape = (1, 56, 56)
+   
+    img = img.reshape(1, 1, 56, 56)
 else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-
-# x_train = x_train.astype('float32')
-# x_test = x_test.astype('float32')
-# x_train /= 255
-# x_test /= 255
-test_images=[]
-test_labels=[]
-
+    input_shape = (56, 56, 1)
+    img = img.reshape(1, 56, 56, 1)
+img = img.astype('float32')
+img /= 255
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
-                 input_shape=(1,56,56)))
+                 input_shape=input_shape))
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -65,15 +48,17 @@ model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(128,activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.25))
-model.add(Dense(nb_class, activation='softmax'))
-
-
-filepath="weights/"+"weights-improvement-{epoch:02d}-{val_acc:.5f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True,mode='max')
-callbacks_list = [checkpoint]
+model.add(Dense(60, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
-model.fit(x_train, y_train, batch_size=batch_size, epochs=120, verbose=1, validation_data=(x_test, y_test),callbacks=callbacks_list)
-
+print("Atkaise ? ")
+#model.load_weights('/home/codehead/Downloads/django_rest_api/imageupload/weights.hdf5')
+model.load_weights('/root/projects/django_rest_api/imageupload/weights_2.hdf5')
+print("Atkaise ? Abar?")
+img_rows, img_cols = 56, 56
+value= model.predict_proba(img)
+print(value)
+value = model.predict_classes(img)
+print(value[0])
