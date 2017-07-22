e5716178841c7d805e4210d6cd090de83cd3060e
Md Shopon <shopon.uap@gmail.com>
2017-07-22 15:40:52 +0600

    LatestCommit

--- rest_api/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class MyPhoto(models.Model):
-    image = models.ImageField(upload_to='photos', max_length=254)
--- rest_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-from .models import MyPhoto
-
-class PhotoSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = MyPhoto
-        fields = '__all__'
